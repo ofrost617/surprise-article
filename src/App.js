23ab8d87a25dd270d0e4c08d4cf84b53849de30e
Olivia Frost <olivia@decoded.com>
2018-05-08 12:08:06 +0100

    extracted article pcker component into it's own file and then imported it to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ArticlePicker from './ArticlePicker'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <img className="thumbnail" src="https://images.pexels.com/photos/920115/pexels-photo-920115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
-        <h2>This will be the title of the article</h2>
-        <h3>This will be the description of the article</h3>
-        <button type="button">Twist</button>
-        <button type="button">Stick</button>
+        <Header/>
+        <ArticlePicker/>
       </div>
     )
   }
@@ -19,4 +17,4 @@ class App extends React.Component {
 
 export default App;
 
-    {/* <p>This is the app component which will contain the article picker component eventually, but where we will build it for now</p> */}
+    // {/* <p>This is the app component which will contain the article picker component eventually, but where we will build it for now</p> */}
