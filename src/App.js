7ae5a930297b2c17ee35cedf2d5b1e09bd938b32
Olivia Frost <olivia@decoded.com>
2018-05-08 11:47:04 +0100

    templated app.js with image, title, description and two buttons

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends React.Component {
     return (
       <div>
         <img className="thumbnail" src="https://images.pexels.com/photos/920115/pexels-photo-920115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
-        <h2>testing</h2>
+        <h2>This will be the title of the article</h2>
+        <h3>This will be the description of the article</h3>
+        <button type="button">Twist</button>
+        <button type="button">Stick</button>
       </div>
     )
   }
