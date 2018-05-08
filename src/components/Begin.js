1351c96896588d99ae822d2e1162c57abfe3ce15
Olivia Frost <olivia@decoded.com>
2018-05-08 14:22:20 +0100

    added onClick event to Begin.js to trigger loading an article and begin

--- src/components/Begin.js
+++ src/components/Begin.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 class Begin extends React.Component {
+  goToArticle() {
+    console.log('Trying to go to article');
+  }
   render() {
     return (
       <div>
         <h1>Welcome to Article Blackjack!</h1>
         <h2>Got lots of saved articles and no time to choose? Let us help you out</h2>
         <p>Click below to begin</p>
-        <button type="button">Give me an article</button>
+        <button type="button" onClick={this.goToArticle}>Give me an article</button>
       </div>
     )
   }
