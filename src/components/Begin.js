43b290bc35f37029576a7ade1c440907809acd04
Olivia Frost <olivia@decoded.com>
2018-05-09 15:41:08 +0100

    added context type to begin.js to allow routing to /article when button is clicked

--- src/components/Begin.js
+++ src/components/Begin.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Begin extends React.Component {
-  goToArticle() {
-    console.log('Trying to go to article');
+  // constructor() {
+  //   super();
+  //   this.goToArticle = this.goToArticle.bind(this);
+  // }
+  // This is the same as .bind(this on in the onClick but I've used the other way ay I'm only going to be rendering the begin component once on the page)
+
+  goToArticle(event) {
+    // event.preventDefault(); do I need this?
+    // using the imperative API rather than redirect component
+    this.props.history.push(`article`)
+
   }
   render() {
     return (
@@ -10,10 +20,14 @@ class Begin extends React.Component {
         <h1>Welcome to Article Blackjack!</h1>
         <h2>Got lots of saved articles and no time to choose? Let us help you out</h2>
         <p>Click below to begin</p>
-        <button type="button" onClick={this.goToArticle}>Give me an article</button>
+        <button type="button" onClick={this.goToArticle.bind(this)}>Give me an article</button>
       </div>
     )
   }
+}
+
+Begin.contextTypes = {
+  router: PropTypes.object
 }
 
 export default Begin;
